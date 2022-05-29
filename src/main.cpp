a2c7efa41df9e5c0e3237994c9cd512ab3a723c4
Riphiphip <halvor.bjorstad@gmail.com>
2022-05-29 17:12:03 +0200

    Add geometry shader for normal visualization

--- src/main.cpp
+++ src/main.cpp
@@ -169,6 +169,25 @@ int main()
 
     duckieNode.children.push_back(&hatNode);
 
+    vector<ShaderPath> duckieNormalsProgramShaders{
+        {
+            .path = "../shaders/default.vert",
+            .type = VERTEX,
+        },
+        {
+            .path = "../shaders/showNormals.geo",
+            .type = GEOMETRY,
+        },
+        {
+            .path = "../shaders/normals.frag",
+            .type = FRAGMENT,
+        },
+    };
+    GLuint duckieNormalsProgram = createProgram(duckieNormalsProgramShaders);
+    scene::MeshNode duckieNormalsNode(duckieMesh,  duckieNormalsProgram);
+
+    duckieNode.children.push_back(&duckieNormalsNode);
+
     PointLightSet tvShowPointLights;
 
     PointLight light2;
